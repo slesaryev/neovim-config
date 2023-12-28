77376b99a4150d7e9744e29bf557a3655b699dc4
Andriy Slesaryev <andriy.slesaryev@gmail.com>
2023-12-28 17:34:44 +0100

    Updated bufferline config

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,39 +1,44 @@
 return {
-	"akinsho/bufferline.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	version = "*",
-	config = function()
-		local icons = require("core.icons")
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*",
+  config = function()
+    local icons = require("core.icons")
 
-		require("bufferline").setup({
-			options = {
-				mode = "buffers",
+    require("bufferline").setup({
+      highlights = {
+        fill = { bg = "NONE", fg = "#ff0000" },
+      },
+      options = {
+        mode = "buffers",
 
-				-- "slant" | "slope" | "thick" | "thin" | { 'any', 'any' },
-				-- separator_style = "slant",
+        -- "slant" | "slope" | "thick" | "thin" | { 'any', 'any' },
+        separator_style = "thin",
 
-				offsets = {
-					{
-						filetype = "NvimTree",
-						text = "File Explorer",
-						highlight = "Directory",
-						separator = true, -- use a "true" to enable the default, or set your own character
-					},
-					{
-						filetype = "Symbols",
-						text = "Outline",
-						highlight = "Directory",
-						separator = true,
-					},
-				},
-			},
-		})
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = icons.file_tree .. " File Explorer",
+            text_align = "left",
+            highlight = "Directory",
+            separator = true, -- use a "true" to enable the default, or set your own character
+          },
+          {
+            filetype = "Outline",
+            text = icons.code .. " Symbols",
+            text_align = "left",
+            highlight = "Directory",
+            separator = true,
+          },
+        },
+      },
+    })
 
-		vim.keymap.set("n", "H", "<cmd>BufferLineCyclePrev<CR>", { desc = icons.chevron_left .. " Previous open buffer" })
-		vim.keymap.set("n", "L", "<cmd>BufferLineCycleNext<CR>", { desc = icons.chevron_right .. " Next open buffer" })
-		vim.keymap.set("n", "<leader>bl", "<cmd>BufferLineCloseLeft<CR>", { desc = icons.arrow_left .. " Close buffers to the left" })
-		vim.keymap.set("n", "<leader>br", "<cmd>BufferLineCloseRight<CR>", { desc = icons.arrow_right .. " Close buffers to the right" })
-		vim.keymap.set("n", "<leader>bc", "<cmd>confirm bwipeout<CR><cmd>BufferLineCyclePrev<CR>", { desc = icons.close .. " Close current buffer" })
-		vim.keymap.set("n", "<leader>bC", "<cmd>BufferLineCloseOther<CR>", { desc = icons.close .. " Close other buffers" })
-	end,
+    vim.keymap.set("n", "H", "<cmd>BufferLineCyclePrev<CR>", { desc = icons.chevron_left .. " Previous open buffer" })
+    vim.keymap.set("n", "L", "<cmd>BufferLineCycleNext<CR>", { desc = icons.chevron_right .. " Next open buffer" })
+    vim.keymap.set("n", "<leader>bl", "<cmd>BufferLineCloseLeft<CR>", { desc = icons.arrow_left .. " Close buffers to the left" })
+    vim.keymap.set("n", "<leader>br", "<cmd>BufferLineCloseRight<CR>", { desc = icons.arrow_right .. " Close buffers to the right" })
+    vim.keymap.set("n", "<leader>bc", "<cmd>confirm bwipeout<CR><cmd>BufferLineCyclePrev<CR>", { desc = icons.close .. " Close current buffer" })
+    vim.keymap.set("n", "<leader>bC", "<cmd>BufferLineCloseOther<CR>", { desc = icons.close .. " Close other buffers" })
+  end,
 }
