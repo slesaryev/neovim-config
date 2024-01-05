57afd057dcff370d27aadf2d0811806f34b18c4c
Andriy Slesaryev <andriy.slesaryev@gmail.com>
2024-01-05 22:27:24 +0100

    Added move line keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -18,6 +18,12 @@ keymap.set("n", "<leader>q", "<cmd>confirm quit<CR>", { desc = icons.quit .. " Q
 keymap.set("n", "|", "<C-w>v", { desc = icons.split_horizontal .. " Split window vertically" })
 keymap.set("n", "\\", "<C-w>s", { desc = icons.split_vertical .. " Split window horizontally" })
 
+-- move line up down
+keymap.set("n", "<M-j>", ":m+1<CR>", { desc = icons.arrow_down .. " Move line down", silent = true, noremap = true })
+keymap.set("n", "<M-k>", ":m-2<CR>", { desc = icons.arrow_up .. " Move line up", silent = true, noremap = true })
+keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv", { desc = icons.arrow_down .. " Move lines down", silent = true, noremap = true })
+keymap.set("v", "<M-k>", ":m '<-2<CR>gv=gv", { desc = icons.arrow_up .. " Move lines up", silent = true, noremap = true })
+
 -- window navigation (commented out, since tmux navigator has the shortcuts already)
 -- keymap.set("n", "<C-l>", "<C-w>l", { desc = icons.arrow_right .. "Window focus right" })
 -- keymap.set("n", "<C-h>", "<C-w>h", { desc = icons.arrow_left .. " Window focus left" })
