59958772d8716341c5c6ac69db9f1a3baeeafbc2
Andriy Slesaryev <andriy.slesaryev@gmail.com>
2024-04-02 11:34:10 +0200

    Updated notes icons

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -2,21 +2,23 @@ local icons = require("core.icons")
 
 return {
   "epwalsh/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
+  version = "*",
   lazy = true,
   ft = "markdown",
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
   keys = {
-    { "<leader>nn", "<cmd>ObsidianNew<CR>", desc = icons.opacity .. " New note" },
-    { "<leader>nd", "<cmd>ObsidianDailies<CR>", desc = icons.opacity .. " Dailies" },
+    { "<leader>nn", "<cmd>ObsidianNew<CR>", desc = icons.add .. " New note" },
+    { "<leader>nd", "<cmd>ObsidianDailies<CR>", desc = icons.calendar .. " Dailies" },
+    { "<leader>nr", "<cmd>ObsidianRename<CR>", desc = icons.edit .. " Rename" },
+    { "<leader>nf", "<cmd>ObsidianSearch<CR>", desc = icons.search .. " Search" },
   },
   opts = {
     workspaces = {
       {
-        name = "personal",
-        path = "~/ObsidianVaults/personal",
+        name = "default",
+        path = "~/ObsidianVault/",
       },
     },
   },
