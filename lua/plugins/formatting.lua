92e37db4219b04998c6602b73ca0109e76ae5714
Andriy Slesaryev <andriy.slesaryev@gmail.com>
2024-04-05 01:58:05 +0200

    Added formatting toggle

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -25,12 +25,45 @@ return {
         typescriptreact = { "prettier" },
         yaml = { "prettier" },
       },
-      format_on_save = {
-        async = false,
-        lsp_fallback = true,
-        timeout_ms = 500,
-      },
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return {
+          async = false,
+          lsp_fallback = true,
+          timeout_ms = 500,
+        }
+      end,
     })
+
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
+
+    vim.keymap.set("n", "<leader>uf", function()
+      if vim.g.disable_autoformat then
+        vim.g.disable_autoformat = false
+        print("Autoformat on save enabled")
+      else
+        vim.g.disable_autoformat = true
+        print("Autoformat on save disabled")
+      end
+    end, { desc = "Toggle autoformat on save" })
 
     vim.keymap.set({ "n", "v" }, "<leader>cF", function()
       conform.format({
